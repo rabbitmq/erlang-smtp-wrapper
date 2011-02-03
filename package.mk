3fc98f4189f5353d37b39472b388ab880daa6c06
David Wragg <david@rabbitmq.com>
2011-02-03 11:54:05 +0000

    Unify wrapper and non-wrapper package app file generation

--- package.mk
+++ package.mk
@@ -1,4 +1,4 @@
 APP_NAME:=erlang_smtp
 
 UPSTREAM_GIT:=http://github.com/tonyg/erlang-smtp.git
-UPSTREAM_APP_FILE=$(CLONE_DIR)/src/$(APP_NAME).app
+ORIGINAL_APP_FILE=$(CLONE_DIR)/src/$(APP_NAME).app
